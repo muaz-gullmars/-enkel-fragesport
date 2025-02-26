a0b4e461b0853b6b71853d34e578136a82eeb935
muaz-gullmars <muaz.hurani@lysekil.se>
2025-02-26 10:49:32 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,29 @@
+
+# Enkel Frågesport
+# Skapat av: Muaz Hurani
+
+# Lista med frågor, alternativ och korrekta svar
+frågor = [
+    {
+        "fråga": "Vilken är Sveriges huvudstad?",
+        "alternativ": ["A. Göteborg", "B. Stockholm", "C. Malmö", "D. Uppsala"],
+        "svar": "B"
+    },
+    {
+        "fråga": "Vilket år blev Sverige medlem i EU?",
+        "alternativ": ["A. 1986", "B. 1990", "C. 1995", "D. 2000"],
+        "svar": "C"
+    },
+    # TODO: Lägg till minst 3 frågor till här
+]
+
+# Initiera poäng
+poäng = 0
+
+# TODO: Skapa en loop som går igenom alla frågor
+
+# TODO: Visa frågan och alternativen för användaren
+
+# TODO: Ta emot användarens svar och kontrollera om det är korrekt
+
+# TODO: Visa användarens slutliga poäng och ett lämpligt meddelande
